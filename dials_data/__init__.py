38f4497c3dfb65e8b92d8ddc41fe6d99bdb1ba93
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-07 10:57:48 +0000

    Add a function to import to project conftest.py
    This adds a '--regression' command line parameter to pytest - but only
    once.

--- dials_data/__init__.py
+++ dials_data/__init__.py
@@ -3,5 +3,17 @@
 """Top-level package for DIALS Regression Data."""
 
 __author__ = """Markus Gerstel"""
-__email__ = 'dials-support@lists.sourceforge.net'
-__version__ = '0.1.0'
+__email__ = "dials-support@lists.sourceforge.net"
+__version__ = "0.1.0"
+
+
+def pytest_addoption(parser):
+    """Adds '--regression' option to pytest exactly once."""
+    if not hasattr(pytest_addoption, "done"):
+        parser.addoption(
+            "--regression",
+            action="store_true",
+            default=False,
+            help="run regression tests. Download data for those tests if required",
+        )
+    setattr(pytest_addoption, "done", True)
