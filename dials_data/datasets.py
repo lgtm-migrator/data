758702697d9f8a9eaad268537bb5ff539204df12
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-12-06 02:06:52 +0000

    dataset definition basics

---
+++ dials_data/datasets.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""Module providing access to all known data set definitions."""
+
+from __future__ import absolute_import, division, print_function
+
+import hashlib
+import pkg_resources
+import yaml
+
+_hashinfo_version = 1
+
+definition = {}
+fileinfo = {}
+fileinfo_dirty = set()
+for definition_file in pkg_resources.resource_listdir("dials_data", "definitions"):
+    if definition_file.endswith(".yml"):
+        dataset_definition = pkg_resources.resource_string(
+            "dials_data", "definitions/" + definition_file
+        )
+        dataset_name = definition_file[:-4]
+        definition[dataset_name] = yaml.load(dataset_definition)
+        dhash = hashlib.sha256()
+        dhash.update(dataset_definition)
+        definition[dataset_name]["hash"] = dhash.hexdigest()
+        try:
+            fileinfo[dataset_name] = yaml.load(
+                pkg_resources.resource_stream(
+                    "dials_data", "definitions/hashinfo/" + definition_file
+                )
+            )
+            if (
+                fileinfo[dataset_name]["definition"] != definition[dataset_name]["hash"]
+                or definition[dataset_name]["version"] < _hashinfo_version
+            ):
+                fileinfo_dirty.add(dataset_name)
+        except IOError:
+            fileinfo_dirty.add(dataset_name)
+            continue
