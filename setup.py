49eb39d4093693ba252de16a119d6aac1824d412
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-12-06 02:14:47 +0000

    pytest is a real dependency
    pytest-runner is not.

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""The setup script."""
-
+import sys
 from setuptools import setup, find_packages
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = ["pyyaml"]
+requirements = ["pytest", "pyyaml"]
+setup_requirements = []
+test_requirements = []
 
-setup_requirements = ["pytest-runner"]
-
-test_requirements = ["pytest"]
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+if needs_pytest:
+    setup_requirements.append("pytest-runner")
 
 setup(
     author="Markus Gerstel",
