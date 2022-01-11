d412c12cde85ac0619bc20c09e8b31bbb1edda00
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-01-11 12:59:49 +0000

    Bump version: 2.2.0 → 2.3.0

--- dials_data/__init__.py
+++ dials_data/__init__.py
@@ -8,6 +8,6 @@ from __future__ import annotations
 __all__: list = []
 __author__ = """Markus Gerstel"""
 __email__ = "dials-support@lists.sourceforge.net"
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 __commit__ = ""
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
